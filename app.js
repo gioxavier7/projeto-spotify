18fa53bc1a4384c1566addfe2075ad72fb89d658
GIOVANNA SOARES XAVIER <24122285@CFP127ALUNO.LOCAL>
2025-02-13 11:11:42 -0300

    iniciando o projeto (organizando a estrutura dos diretórios, fazendo as função DAO

---
+++ app.js
@@ -0,0 +1,14 @@
+/**
+ * Objetivo: API responsável pelas requisições do projeto de controle de músicas
+ * Data: 13/02/2025
+ * Dev: giovanna
+ * Versões: 1.0
+ * Observações: 
+ * ****Para criar a API precisamos instalar:
+ *      express           npm install express --save
+ *      cors              npm install cors --save
+ *      body-parser       npm install body-parser --save
+ * ****Para criar a conexão com o banco de dados MYSQL precisamos instalar:
+ *      prisma            npm install prisma --save
+ *      prisma/client     npm install prisma/client --save      
+ */
