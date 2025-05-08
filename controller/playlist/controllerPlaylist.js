b419d9f51da4cd47692355a16cbb07ecbebea08a
GIOVANNA SOARES XAVIER <24122285@CFP127ALUNO.LOCAL>
2025-05-08 12:05:45 -0300

    iniciando os relacionamento 1xN

---
+++ controller/playlist/controllerPlaylist.js
@@ -0,0 +1,195 @@
+/**
+  * Objetivo: Controller responsável pela manipilação do CRUD de dados de playlist
+  * Data: 17/04/2025
+  * Dev: Giovanna
+  * Versão: 1.0
+  */
+
+//import do arquivo de configurações de mensagens de status cpde
+const MESSAGE = require('../../modulo/config.js')
+ 
+//import do arquivo DAO de música para manipular o db
+const playlistDAO = require('../../model/dao/playlist.js')
+const { json } = require('body-parser')
+
+//Import das controlleres para criar as relações com a playlist
+const controllerUsuario = require('../usuario/controllerUsuario.js')
+
+//funcao pra inserir um novo playlist
+const inserirPlaylist = async function(playlist, contentType){
+    try {
+        if(String(contentType).toLowerCase() == 'application/json')
+        {
+            if(
+                playlist.titulo == undefined || playlist.titulo == '' || playlist.titulo == null || playlist.titulo.length > 50 ||
+                playlist.duracao == undefined || playlist.duracao == '' || playlist.duracao == null || playlist.duracao.length > 45 ||
+                playlist.descricao == undefined || playlist.descricao == '' || playlist.descricao == null || playlist.descricao.length > 75 ||
+                playlist.id_usuario == undefined || playlist.id_usuario == ''
+            ){
+                return MESSAGE.ERROR_REQUIRE_FIELDS //400
+            }else{
+                let resultPlaylist = await playlistDAO.insertPlaylist(playlist)
+
+                if(resultPlaylist)
+                    return MESSAGE.SUCCESS_CREATED_ITEM //201
+                else
+                    return MESSAGE.ERROR_INTERNAL_SERVER_MODEL //500
+            }
+        }else{
+            return MESSAGE.ERROR_CONTENT_TYPE //415
+        }
+    } catch (error) {
+        return MESSAGE.ERROR_INTERNAL_SERVER_CONTROLLER //500
+    }
+}
+ // funcao pra tratar o retorno de uma lista de playlist do dao
+const listarPlaylist = async function(){
+    try {
+
+        // objeto do tipo array pra utilizar no foreach pra carregar os dados da playlist e do usuario
+        const arrayPlaylist = []
+
+        // objeto json
+        let dadosPlaylist = {}
+
+        //chamar a função que retorna as musicas
+        let resultPlaylist = await playlistDAO.selectAllPlaylist()
+
+        if(resultPlaylist != false || typeof(resultPlaylist) == 'object')
+        {
+            //criando um objeto JSON para retornar a lista de Playlists
+            if(resultPlaylist.length > 0){
+                dadosPlaylist.status = true
+                dadosPlaylist.status_code = 200
+                dadosPlaylist.item = resultPlaylist.length
+                
+                // for of pra trabalhar com requisição async e await
+                for(itemPlaylist of resultPlaylist){
+                    // busca os dados do usuarui na controller usuario utilizando o id do usuario (chave estrangeira)
+                    let dadosUsuario = await controllerUsuario.buscarUsuario(itemPlaylist.id_usuario)
+
+                    //adicionando um atributo "usuario" no json de playlists
+                    itemPlaylist.usuario = dadosUsuario.usuario
+
+                    //remove o atributo id_usuario do json de playlist, pois ja tem o id dentro dos dados de usuario
+                    delete itemPlaylist.id_usuario
+
+                    //adiciona o json do filme, agoa com os dados de usuario em um array
+                    arrayPlaylist.push(itemPlaylist)
+                }
+                //adiciona o novo array de filmes no json o
+            }else{
+                return MESSAGE.ERROR_NOT_FOUND //404
+            }
+        }else{
+            return MESSAGE.ERROR_INTERNAL_SERVER_MODEL //500
+        }
+
+    } catch (error) {
+        return MESSAGE.ERROR_INTERNAL_SERVER_CONTROLLER //500
+    }
+}
+
+//função para listar um Playlist pelo ID
+const buscarPlaylist = async function(id){
+    try {
+        if(id == '' || id == undefined || id == null || isNaN(id) || id <= 0){
+            return MESSAGE.ERROR_REQUIRE_FIELDS //400
+        }else{
+            let dadosPlaylist = {}
+            let resultPlaylist = await playlistDAO.selectByIdPlaylist(id)
+
+            if(resultPlaylist != false || typeof(resultPlaylist) == 'object'){
+                if(resultPlaylist.length > 0){
+                    dadosPlaylist.status = true
+                    dadosPlaylist.status_code = 200
+                    dadosPlaylist.playlist = resultPlaylist
+                    return dadosPlaylist //200
+                }else{
+                    return MESSAGE.ERROR_NOT_FOUND //404
+                }
+            }else{
+                return MESSAGE.ERROR_INTERNAL_SERVER_MODEL //500
+            }
+        }
+    } catch (error) {
+        return MESSAGE.ERROR_INTERNAL_SERVER_CONTROLLER //500
+    }
+}
+
+//função para atualizar um playlist existente
+const atualizarPlaylist = async function(playlist, id, contentType){
+    try {
+        if(String(contentType).toLowerCase() == 'application/json')
+        {
+            if(
+                playlist.titulo == undefined || playlist.titulo == '' || playlist.titulo == null || playlist.titulo.length > 50 ||
+                playlist.duracao == undefined || playlist.duracao == '' || playlist.duracao == null || playlist.duracao.length > 45 ||
+                playlist.descricao == undefined || playlist.descricao == '' || playlist.descricao == null || playlist.descricao.length > 75 ||
+                playlist.id_usuario == undefined || playlist.id_usuario == '' ||
+                id == '' || id == undefined || id == null || isNaN(id) || id <= 0
+            ){
+                return MESSAGE.ERROR_REQUIRE_FIELDS //400
+            }else{
+                //validar se o id existe no db
+                let resultPlaylist = await buscarPlaylist(id)
+
+                if(resultPlaylist.status_code == 200){
+                    //update
+                    playlist.id = id //adiciona o atributo id no json e e coloca o id da música que chegou na controller
+                    let result = await playlistDAO.updatePlaylist(playlist)
+
+                    if(result){
+                        return MESSAGE.SUCCESS_UPDATED_ITEM //200
+                    }else{
+                        return MESSAGE.ERROR_INTERNAL_SERVER_MODEL //500
+                    }
+                }else if(resultPlaylist.status_code == 404){
+                    return MESSAGE.ERROR_NOT_FOUND //404
+                }else{
+                    return MESSAGE.ERROR_INTERNAL_SERVER_CONTROLLER //500
+                }
+            }
+        }else{
+            return MESSAGE.ERROR_CONTENT_TYPE //415
+        }
+    } catch (error) {
+        return MESSAGE.ERROR_INTERNAL_SERVER_CONTROLLER //500
+    }
+}
+
+//função para excluir uma playlist existente
+const excluirPlaylist = async function(id){
+    try {
+        if(id == '' || id == undefined || id == null || isNaN(id) || id <= 0){
+            return MESSAGE.ERROR_REQUIRE_FIELDS //400
+        }else{
+            // validar se o ID existe
+            let resultPlaylist = await buscarPlaylist(id)
+
+            if(resultPlaylist.status_code == 200){
+                // delete do user
+                let result = await playlistDAO.deletePlaylist(id)
+                if(result){
+                    return MESSAGE.SUCCESS_DELETED_ITEM //200
+                }else{
+                    return MESSAGE.ERROR_INTERNAL_SERVER_MODEL //500
+                }
+            }else if(resultPlaylist.status_code == 404){
+                return MESSAGE.ERROR_NOT_FOUND //404
+            }else{
+                return MESSAGE.ERROR_INTERNAL_SERVER_CONTROLLER //500
+            }
+        }
+    } catch (error) {
+        return MESSAGE.ERROR_INTERNAL_SERVER_CONTROLLER //500
+    }
+}
+
+module.exports = {
+    inserirPlaylist,
+    listarPlaylist,
+    buscarPlaylist,
+    atualizarPlaylist,
+    excluirPlaylist
+}
---
+++ model/dao/playlist.js
@@ -0,0 +1,126 @@
+/**
+ * objetivo: model responsável pelo CRUD de dados de playlist no banco de dados
+ * data: 24/04/2025
+ * dev: giovanna
+ * versão: 1.0
+ */
+
+//import da biblioteca Prisma/Client
+const { PrismaClient } = require('@prisma/client')
+
+//instanciando (criar um novo objeto) para realizar a manipulação do script SQL
+const prisma = new PrismaClient()
+
+//funcao para inserir um playlist no bando de dados
+const insertPlaylist = async function(playlist){
+    try {
+        let sql = `insert into tbl_playlist(
+                                            titulo,
+                                            duracao,
+                                            descricao
+                                            )
+                                      values(
+                                             '${playlist.titulo}',
+                                             '${playlist.duracao}',
+                                             '${playlist.descricao}',
+                                             '${playlist.id_usuario}'
+                                             )`
+
+        let result = await prisma.$executeRawUnsafe(sql)
+
+        if(result)
+            return true
+        else
+        return false
+
+    } catch (error) {
+        console.log(error);
+        
+        return false
+    }
+}
+
+//função para atualizar um Playlist existente no banco de dados
+const updatePlaylist = async function(playlist){
+    try {
+        let sql = `update tbl_playlist set titulo= '${playlist.titulo}',
+                                            duracao= '${playlist.duracao}',
+                                            descicao= '${playlist.descicao}',
+                                            id_usuario= '${playlist.id_usuario}'
+                                        where id=${playlist.id}`
+                        
+        let result = await prisma.$executeRawUnsafe(sql)
+
+        if(result)
+            return true
+        else
+            return false
+
+    } catch (error) {
+        return false
+    }
+}
+
+//função para excluir um Playlist existente no banco de dados
+const deletePlaylist = async function(id){
+    try {
+        //script sql
+        let sql = 'delete from tbl_playlist where id='+id
+
+        //executa o script
+        let result = await prisma.$executeRawUnsafe(sql)
+
+        if(result)
+            return true
+        else
+            return false
+
+    } catch (error) {
+        return false
+    }
+}
+
+//função para retornar todas as Playlist do banco de dados
+const selectAllPlaylist = async function(){
+    try {
+        let sql = 'select * from tbl_playlist order by id desc'
+
+        //executa o script sql no db e aguarda o retorno dos dados
+        let result = await prisma.$queryRawUnsafe(sql)
+
+        if(result)
+            return result
+        else
+            return false
+
+    } catch (error) {
+        return false
+    }
+}
+
+//função para listar um Playlist pelo ID no banco de dados
+const selectByIdPlaylist = async function(id){
+    try {
+        //script sql
+        let sql = 'select * from tbl_playlist where id='+id
+
+        //executa o script
+        let result = await prisma.$queryRawUnsafe(sql)
+
+        if(result)
+            return result
+        else
+            return false
+
+    } catch (error) {
+        return false
+    }
+}
+
+module.exports = {
+    insertPlaylist,
+    updatePlaylist,
+    deletePlaylist,
+    selectAllPlaylist,
+    selectByIdPlaylist
+}
